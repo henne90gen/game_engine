8d86b2f8001c2564dc4b53c8680c0c5f3b06ae87
Henne <henne90gen@gmail.com>
2018-10-31 00:19:20 +0100

    Adding texture

--- game_engine/asset.py
+++ game_engine/asset.py
@@ -1,6 +1,20 @@
 from typing import List
 from game_engine.vertex_objects import VBO, VertexAttribute, Uniform
 
+import cv2
+
+
+class Texture:
+    def __init__(self, image):
+        self.image = image
+        self.width = image.cols
+        self.height = image.rows
+
+
+def load_image(file_name: str):
+    img = cv2.imread(file_name)
+    return Texture(img)
+
 
 class Mesh:
     def __init__(self, vbos: List[VBO], vertex_attributes: List[VertexAttribute], uniforms: List[Uniform]):
